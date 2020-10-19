ea00ef2a79fb3c51d7f0a82a87a619d859912c70
Ivan <vantank36@gmial.com>
2020-10-20 00:48:26 +0300

    Scaled coefs.

--- calculate_coef.py
+++ calculate_coef.py
@@ -7,8 +7,12 @@ for i in range(8):
 	raw_data += data['original_size'][i]
 	ari_compressed_data += data['compressed_size'][i]
 	ppm_compressed_data += data['compressed_size'][i+8]
+file_2 = data['compressed_size'][1]/data['original_size'][1]
+file_3 = data['compressed_size'][2]/data['original_size'][2]
 print(data)
 print("ariphmetic compression coeficient: ", "%.3f" % (ari_compressed_data/raw_data))
-print("ariphmetic compression on 2 & 3 files: ", "%.3f" % ((data['compressed_size'][1] + data['compressed_size'][2])/(data['original_size'][1] + data['original_size'][2])))
+print("ariphmetic compression on 2 & 3 files: ", "%.3f" % ((file_2+file_3)/2))
+file_2 = data['compressed_size'][8]/data['original_size'][8]
+file_3 = data['compressed_size'][9]/data['original_size'][9]
 print("ppm compression coeficient: ", "%.3f" % (ppm_compressed_data/raw_data))
-print("ariphmetic compression on 2 & 3 files: ", "%.3f" % ((data['compressed_size'][9] + data['compressed_size'][10])/(data['original_size'][9] + data['original_size'][10])))
+print("ppm compression on 2 & 3 files: ", "%.3f" % ((file_2+file_3)/2))
